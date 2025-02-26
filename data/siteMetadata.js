a72a5de1c34ab2d4b2cddc0e2c288262e8b49166
Lucas <xianminx@gmail.com>
2025-02-26 19:48:30 +0800

    feat: update site metadata for new blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Asylum',
+  author: "Lukas' Blog",
+  headerTitle: 'Asylum',
+  description: 'Blog of Lukas',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://xianminx.github.io/',
