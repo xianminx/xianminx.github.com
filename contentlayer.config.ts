a8ac9524b1d5965c576af0f4b78db46b78bc0a91
Lucas <xianminx@gmail.com>
2025-03-12 12:21:35 +0800

    feat(blog): add image field to blog document type

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -105,6 +105,7 @@ export const Blog = defineDocumentType(() => ({
     draft: { type: 'boolean' },
     summary: { type: 'string' },
     images: { type: 'json' },
+    image: { type: 'string' },
     authors: { type: 'list', of: { type: 'string' } },
     layout: { type: 'string' },
     bibliography: { type: 'string' },
